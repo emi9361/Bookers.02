a44f081881f31189bfc4cda7b30e89e43cfe31f5
vagrant <vagrant@localhost.localdomain>
2020-06-01 14:21:06 +0000

    rev

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,27 @@
 class UsersController < ApplicationController
+
+  def index
+  end
+
   def show
   	@user = User.find(params[:id])
-  	@books =@user.book.page(params[:page]).reverse_order
+  	@books =@user.books
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
   end
 
   def edit
-  	@user = User.find(parmas[:id])
+  	@user = User.find(params[:id])
   end
 
   def update
-	@user = User.find(params[:id])
+	  @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end
@@ -18,4 +30,9 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
+
+  def book_params
+    params.require(:user).permit(:title, :body)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-   root 'homes#about'
+   root 'homes#top'
+   get 'home/about' => 'homes#about'
    devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    resources :books, only:[:create, :index, :show, :destroy]
-   resources :users, only: [:show, :edit, :update]
+   resources :users, only:[:index, :show, :edit, :update]
 
 end
